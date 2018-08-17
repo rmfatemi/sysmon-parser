f96cd732dc002107114e368d6531cebbd9918b8d
Rasool Fatemi <35937118+rmfatemi@users.noreply.github.com>
2018-08-17 13:45:04 -0300

    Add files via upload

---
+++ sysmonparser.py
@@ -0,0 +1,98 @@
+import csv
+import pandas
+import sys
+import commands
+import re
+
+# # Getting the input CSV file as an argument as well as the number of lines in it
+# output = commands.getoutput('wc -l ' + str(sys.argv[-1]))
+# num_lines, input_file = output.split()
+
+# # Droping unnecessary rows and columns out of the raw input data
+# raw_data = pandas.read_csv(input_file, delimiter='\r\n', engine='python')
+# raw_data.to_csv('processed.csv', header=None, index=False)
+
+def getRule(str1):
+    substrings = str1.split()
+    rule = substrings[substrings.index('(rule:') + 1]
+    rule = rule.split(')', 1)[0]
+    rule = re.findall('[A-Z][^A-Z]*', rule)
+    rule = rule[0] + ' ' + rule[1]
+    return rule
+
+def getDate(str1):
+    substrings = str1.split()
+    date = substrings[1]
+    return date
+
+def getTime(str1):
+    substrings = str1.split()
+    time = substrings[2]
+    return time
+
+def getHash(str1):
+    substrings = str1.split(',')
+    MD5 = substrings[0]
+    MD5 = MD5.replace('Hashes: MD5=', '')
+    MD5 = MD5.replace('Hash: MD5=', '')
+    return MD5
+
+def getCommandLine(str1):
+    command = str1.replace('CommandLine: ' , '')
+    return command
+
+def getGUID(str1):
+    guid = str1.replace('ProcessGuid: {', '')
+    guid = guid.replace('}', '')
+    return guid
+
+def getParentGUID(str1):
+    parentguid = str1.replace('ParentProcessGuid: {', '')
+    parentguid = parentguid.replace('}', '')
+    return parentguid
+
+def getLogonGUID(str1):
+    logonguid = str1.replace('LogonGuid: {', '')
+    logonguid = logonguid.replace('}', '')
+    return logonguid
+
+
+with open('processed.csv') as csvin, open('final.csv', mode='w') as csvout:
+    
+    processed_data = csv.reader(csvin)
+    final_data = csv.writer(csvout)
+
+    header = ['Date', 'Time', 'Rule', 'MD5 Hash', 'Process GUID', 'Parent Process GUID', 'Logon ID']#, 'Command Line']
+    final_data.writerow(header)
+
+    event = [None] * len(header)
+
+    for row in processed_data:
+
+        str1 = ''.join(row)
+        str1 = str1.replace('"', '')
+
+        if ('UtcTime' in str1):
+            event[0] = getDate(str1)
+            event[1] = getTime(str1)
+            
+        elif ('(rule:' in str1):
+            event[2] = getRule(str1)
+            
+        elif ('MD5' in str1):
+            event[3] = getHash(str1)
+
+        elif (str1.startswith('ProcessGuid: {')):
+            event[4] = getGUID(str1)
+            
+        elif (str1.startswith('ParentProcessGuid: {')):
+            event[5] = getParentGUID(str1)
+
+        elif (str1.startswith('LogonGuid: {')):
+            event[6] = getLogonGUID(str1)
+
+        # elif (str1.startswith('CommandLine: ')):
+        #     event[7] = getCommandLine(str1)
+
+        if (str1.startswith('Information') and event[0]):
+            final_data.writerow(event)
---
+++ vtchecker.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+import time
+
+with open('final.csv') as csvin:
+
+    data = csv.reader(csvin)
+    next(data)
+    hashes = set()
+    
+    for row in data:
+        hash = ''.join(row[3])
+        hashes.add(hash)
+    hashes = list(hashes)
+
+    for md5 in hashes:
+        params = {'apikey': '128a4fbb3e281a9f023bc851d054a8d1c98f65cca977cf314d45d07a609e15e4', 'resource': md5}
+        try:
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params).json()
+            positives = response.get('positives', None)
+            total = response.get('total', None)
+            print(md5, positives, total, 'malicious' if positives else 'clean')
+        except:
+            time.sleep(59)
